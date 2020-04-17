227ab037649b3b057e365f6537936a54dab536ec
igorek5 <igorbautin101@gmail.com>
2020-04-17 18:39:42 +0300

    Добавил ссылки BrowserRoute

--- src/App.js
+++ src/App.js
@@ -3,17 +3,30 @@ import './App.css';
 import Header from './components/Header/Header';
 import Navbar from './components/Navbar/Navbar';
 import Profile from './components/Profile/Profile';
+import Dialogs from "./components/Dialogs/Dialogs";
+import News from "./components/News/News";
+import Music from "./components/Music/Music";
+import Settings from "./components/Settings/Settings";
+import {BrowserRouter, Route} from "react-router-dom";
 
 const App = () => {
-  return (<div className='main__wrapper'>
-    <Header />
-    <div className='container content__container'>
-      <Navbar />
-      <Profile />
-    </div>
-  </div>
+    return (
+        <BrowserRouter>
+            <div className='main__wrapper'>
+                <Header/>
+                <div className='container container__flex'>
+                    <Navbar />
 
-  );
+                    <Route path='/profile' component={Profile}/>
+                    <Route path='/dialogs' component={Dialogs}/>
+                    <Route path='/news' component={News}/>
+                    <Route path='/music' component={Music}/>
+                    <Route path='/settings' component={Settings}/>
+               
+                </div>
+            </div>
+        </BrowserRouter>
+    );
 }
 
 
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,30 +2,29 @@ import React from 'react';
 import s from './Navbar.module.css'
 
 const Navbar = () => {
-    return <aside className={s.nav_bar}>
-        <nav>
-            <ul className={s.nav}>
-                <li className={s.nav__item}>
-                    <a href='#c'>Profile</a>
-                </li>
-                <li className={s.nav__item}>
-                    <a href='#c'>Messages</a>
-                </li>
-                <li className={s.nav__item}>
-                    <a href='#c'>News</a>
-                </li>
-                <li className={s.nav__item}>
-                    <a href='#c'>Music</a>
-                </li>
-                <li className={s.nav__item}>
-                    <a href='#c'>Profile</a>
-                </li>
-                <li className={`${s.nav__item} ${s.nav__settings}`} >
-                    <a href='#c'>Settings</a>
-                </li>
-            </ul>
-        </nav>
-    </aside>
+    return (
+        <aside className={s.nav_bar}>
+            <nav>
+                <ul className={s.nav}>
+                    <li className={s.nav__item}>
+                        <a href='/profile'>Profile</a>
+                    </li>
+                    <li className={s.nav__item}>
+                        <a href='/dialogs'>Messages</a>
+                    </li>
+                    <li className={s.nav__item}>
+                        <a href='/News'>News</a>
+                    </li>
+                    <li className={s.nav__item}>
+                        <a href='/Music'>Music</a>
+                    </li>
+                    <li className={`${s.nav__item} ${s.nav__settings}`}>
+                        <a href='/Settings'>Settings</a>
+                    </li>
+                </ul>
+            </nav>
+        </aside>
+    )
 }
 
 export default Navbar;
