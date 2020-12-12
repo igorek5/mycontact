a3a90cb094eb67987984278160a083a4134555a2
igorek5 <igorbautin101@gmail.com>
2020-12-12 16:33:06 +0300

    Updates

---
+++ precache-manifest.8018ad8c787d5d5a39b0fe824a054e15.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce4add5b911d9b90a52920b01f9e4950",
+    "url": "/mycontact/index.html"
+  },
+  {
+    "revision": "208671a9ce681606d363",
+    "url": "/mycontact/static/css/3.68d25050.chunk.css"
+  },
+  {
+    "revision": "14b3b22112ce08e73c57",
+    "url": "/mycontact/static/css/main.9b0ec643.chunk.css"
+  },
+  {
+    "revision": "e471e4288d93534e4437",
+    "url": "/mycontact/static/js/2.eebdc9f2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/mycontact/static/js/2.eebdc9f2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "208671a9ce681606d363",
+    "url": "/mycontact/static/js/3.21482389.chunk.js"
+  },
+  {
+    "revision": "14b3b22112ce08e73c57",
+    "url": "/mycontact/static/js/main.e30ee6cc.chunk.js"
+  },
+  {
+    "revision": "266e3ec74c4baf152653",
+    "url": "/mycontact/static/js/runtime-main.4b7b2c05.js"
+  },
+  {
+    "revision": "12b75343d83cc27d376df861aadb93aa",
+    "url": "/mycontact/static/media/metaballs.12b75343.gif"
+  }
+]);
